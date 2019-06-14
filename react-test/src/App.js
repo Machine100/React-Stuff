7b6b27a83d518283e0d18ac515c21774052afd80
Machine100 <joesmileyyy@yahoo.com>
2019-06-14 16:28:40 -0400

    .

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-//import './App.css';
+import './App.css';
 import ChildComponent from './Apptest'
+
 
 class App extends React.Component {
   
@@ -8,13 +9,9 @@ class App extends React.Component {
     
     return (
       <div>
-        <ul>
-          <li>This is App.js calling several components</li>
-          <li>Being able to do this was one of my goals.</li>
-          <li>arrange components and make them look good.</li>
-          <li>Or maybe I'll do a login form for Okta or Cognito.</li>
-          <ChildComponent />
-        </ul>
+        
+        <ChildComponent />
+        <button className="button1"> buttontext </button>
       </div>
     );
   }
