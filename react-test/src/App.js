b5dbedbe318d2ed8461ff460bbf2f442626def15
machine100 <joesmileyyy@yahoo.com>
2019-06-15 05:19:38 -0700

    .

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -10,8 +10,21 @@ class App extends React.Component {
     return (
       <div>
         
+        <div className="buttongrid">
+          <button className="button1"> button1 </button>
+          <button className="button1"> button2 </button>
+          <button className="button1"> button3 </button>
+          <button className="button1"> button4 </button>
+          <button className="button1"> button5 </button>
+          <button className="button1"> button6 </button>
+          <button className="button1"> button7 </button>
+          <button className="button1"> button8 </button>
+          <button className="button1"> button9 </button>
+        </div>
+
+        <h1> put these buttons in css-grid </h1>
+
         <ChildComponent />
-        <button className="button1"> buttontext </button>
       </div>
     );
   }
