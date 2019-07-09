72514adccbf7a614e2c7eb1c8b29dac47756963f
machine100 <joesmileyyy@yahoo.com>
2019-07-08 19:43:39 -0700

    .

--- react-form/src/index.js
+++ react-form/src/index.js
@@ -4,38 +4,6 @@ import './index.css';
 import App from './App';
 
 
-const dog = {
-	sound:'bark',
-	makenoise: function(){console.log(this.sound)}
-	}
-
-console.log (dog)
-console.log (dog.sound)
-console.log (dog.makenoise)
-dog.makenoise()
-
-function cat (){
-	var noise = 'meow'
-	var i = 0
-	function makenoise(){console.log(noise, i); i++}
-	return makenoise
-	}
-
-var y = cat()
-console.log (y)
-y()
-y()
-y()
-
-
-
-//console.log (cat)
-//console.log (cat.sound)
-//console.log (cat.makenoise)
-//cat.makenoise()
-// var y = cat.makenoise()
-// y()
-
 
 
 
