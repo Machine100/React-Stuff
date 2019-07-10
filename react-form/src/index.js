5e0b0e5f7f39cc004b7491424329ed9710d71260
Machine100 <joesmileyyy@yahoo.com>
2019-07-10 08:22:16 -0400

    .

--- react-form/src/index.js
+++ react-form/src/index.js
@@ -4,6 +4,30 @@ import './index.css';
 import App from './App';
 
 
+const dog = {
+	sound: 'bark',
+	makenoise: function(){console.log(this.sound)}
+}
+
+console.log (dog.sound)
+console.log (dog.makenoise)
+dog.makenoise()
+var x = dog.makenoise.bind(dog)
+console.log (x)
+x()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
